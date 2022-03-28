d930cf068c6ef86042b9ae0b0379fcb5b4d9c7bf
Annika Neumann <annika1983neumann@gmail.com>
2022-03-28 20:41:58 +0100

    episode 1-3 completed

---
+++ output_scope_whodunnit.js
@@ -0,0 +1,78 @@
+// ### MVP
+
+// #### Episode 1
+
+
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+
+// the murderer is Miss Scarlet, because the object scenario is a global scope. It can be accessed by function and because she is the only
+// murderer named here, it can only be her
+
+
+// #### Episode 2
+
+
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// The murderer is Professor Plum or it would come back with an error, because a const variable cannot be reassigned with another value.
+// It cannot be Mrs Peacock, lucky her.
+
+#### Episode 3
+
+
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+
+// First verdict would be that Mrs Peacock is the murderer, because the variable takes in the variable which is within the function 
+// with her name.
+// Second verdict is Professor Plum because the variable can use the one defined globally.
+
+#### Episode 4
+
+
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
